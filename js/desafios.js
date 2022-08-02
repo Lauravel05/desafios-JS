4b152667bf8ca745b8f73d7e35e022d4eeab77e6
Laura Velandia <lauravelandia2003@gmail.com>
2022-08-02 12:46:23 -0500

    Primer desafio

---
+++ js/desafios.js
@@ -0,0 +1,13 @@
+let cP = parseInt(prompt("Indique cuantas personas registrará"));
+let estatura;
+let sE = 0;
+let n;
+
+for (n=1; n<=cP; n++){
+    estatura = parseInt(prompt("Ingrese la estatura en centímetros de la persona " + n));
+
+    sE = sE + estatura;
+}
+
+
+console.log("El promedio de estatura de las", cP, "personas es de:", sE/cP, "centímetros.")
